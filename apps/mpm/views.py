6124cc77caee015a00d11398acdd4b89f1d03dbd
Gustavo Furtado <gustavo.foa@gmail.com>
2015-11-10 17:46:42 -0200

    Config Sidebar and content area

--- apps/mpm/views.py
+++ apps/mpm/views.py
@@ -5,16 +5,26 @@ from django.shortcuts import get_object_or_404, render
 
 from .models import Musica, Categoria
 
+def base_context():
+	tempos = Categoria.objects.filter(categoria_mae=None,slug__startswith="tempo");
+	especiais = Categoria.objects.filter(categoria_mae=None).exclude(slug__startswith="tempo");
+	ctx = {'tempos': tempos, 'especiais': especiais}
+	return ctx
+
 def index(request):
 	return render(request, 'index.html', {})
 
 def musica(request, slug):
+	ctx = base_context();
 	musica = get_object_or_404(Musica, slug=slug)
-	return render(request, 'musica.html', {'musica': musica})
+	ctx['musica'] = musica
+	return render(request, 'musica.html', ctx)
 
 def musicas_de(request, slug):
+	ctx = base_context();
 	categoria = get_object_or_404(Categoria, slug=slug)
-	return render(request, 'musicas-de.html', {'categoria': categoria})
+	ctx['categoria'] = categoria
+	return render(request, 'musicas-de.html', ctx)
 
 def sugestoes_para(request):
 	return render(request, 'sugestoes-para.html', {})
