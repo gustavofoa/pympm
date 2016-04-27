fdad055042c4cafe34e6ef64a663c46b545555cd
Gustavo Furtado <gustavo.foa@gmail.com>
2016-04-27 10:51:16 -0300

    Fix 404 static files path

--- apps/mpm/forms.py
+++ apps/mpm/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class ImportData(forms.Form):
-    url_categorias = forms.URLField(label='URL categorias')
-    url_musicas = forms.URLField(label='URL musicas')
-    url_sugestoes = forms.URLField(label='URL sugestoes')
-    url_datas = forms.URLField(label='URL datas')
+    url_categorias = forms.URLField(label='URL categorias',required=False)
+    url_musicas = forms.URLField(label='URL musicas',required=False)
+    url_sugestoes = forms.URLField(label='URL sugestoes', required=False)
+    url_datas = forms.URLField(label='URL datas', required=False)
