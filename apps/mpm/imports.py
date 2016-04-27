0d39c578fecdd8395ac9b8d8496432aff8d818fc
Gustavo Furtado <gustavo.foa@gmail.com>
2016-04-27 11:10:08 -0300

    fix print encoding

--- apps/mpm/imports.py
+++ apps/mpm/imports.py
@@ -124,7 +124,7 @@ class ImportPaginasSugestoes:
                         if ret:
                             i.remover.add(Musica.objects.get(slug=ret.strip()))
                     i.save()
-                print ">>> ", posicao, " - ", item["title"]
+                print ">>> Importando Pg. ", posicao#, " - ", item["title"]
                 posicao = posicao + 1
 
             cont = cont + 1
