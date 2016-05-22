2a7a5cb66c053065136e8b2e9fd5d7927c40f461
Gustavo Furtado <gustavo.foa@gmail.com>
2016-05-22 20:23:58 -0300

    Musica tools events mapping

---
+++ apps/mpm/static/js/musica-tools.js
@@ -0,0 +1,31 @@
+$('#print-letra').on('click', function (e) {
+     alert('imprimir letra')
+});
+
+$('#remove-font-size-letra').on('click', function (e) {
+     alert('diminuir fonte letra')
+});
+
+$('#add-font-size-letra').on('click', function (e) {
+     alert('aumentar fonte letra')
+});
+
+$('#remove-meio-tom-cifra').on('click', function (e) {
+     alert('diminuir tom cifra')
+});
+
+$('#add-meio-tom-cifra').on('click', function (e) {
+     alert('aumentar tom cifra')
+});
+
+$('#print-cifra').on('click', function (e) {
+     alert('imprimir cifra')
+});
+
+$('#remove-font-size-cifra').on('click', function (e) {
+     alert('diminuir fonte cifra')
+});
+
+$('#add-font-size-cifra').on('click', function (e) {
+     alert('aumentar fonte cifra')
+});
