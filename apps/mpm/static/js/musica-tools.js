1cf7d7e37652d70fdb29b3400c905cd6fe98ea09
Gustavo Furtado <gustavo.foa@gmail.com>
2016-05-22 22:02:09 -0300

    Print function

--- apps/mpm/static/js/musica-tools.js
+++ apps/mpm/static/js/musica-tools.js
@@ -1,31 +1,75 @@
 $('#print-letra').on('click', function (e) {
-     alert('imprimir letra')
+  printPopup($('#titulo-musica').html(), '<div id="letra">'+$('#div-letra').html()+'</div>');
 });
 
 $('#remove-font-size-letra').on('click', function (e) {
-     alert('diminuir fonte letra')
+
 });
 
 $('#add-font-size-letra').on('click', function (e) {
-     alert('aumentar fonte letra')
+
 });
 
 $('#remove-meio-tom-cifra').on('click', function (e) {
-     alert('diminuir tom cifra')
+
 });
 
 $('#add-meio-tom-cifra').on('click', function (e) {
-     alert('aumentar tom cifra')
+
 });
 
 $('#print-cifra').on('click', function (e) {
-     alert('imprimir cifra')
+  printPopup($('#titulo-musica').html(), '<pre id="cifra">'+$('#div-cifra').html()+'</pre>');
 });
 
 $('#remove-font-size-cifra').on('click', function (e) {
-     alert('diminuir fonte cifra')
+
 });
 
 $('#add-font-size-cifra').on('click', function (e) {
-     alert('aumentar fonte cifra')
+
 });
+
+function printPopup(title, data)
+{
+    var mywindow = window.open('', title);
+    mywindow.document.write('<html><head><title>'+title+' - Músicas para Missa</title>');
+    mywindow.document.write('<style>');
+    mywindow.document.write('#letra {'+
+'            font-size: 20px;'+
+'            background: white;'+
+'            display: block;'+
+'            padding-right: 85px;'+
+'    }'+
+'    #cifra {'+
+'    	font-size: 18px;'+
+'    	background: white;'+
+'    	font-family: monospace;'+
+'    	display: block;'+
+'    	padding-right: 85px;'+
+'    }'+
+'    #cifra b {'+
+'    	color: #FF0000;'+
+'    	font-weight: bold;'+
+'    }'+
+'    img {'+
+'        position: absolute;'+
+'        width: 100%;'+
+'        padding-top: 200px;'+
+'        opacity: 0.1;'+
+'        filter: alpha(opacity=10);'+
+'    }'+
+'    h2 {'+
+'    	padding-right: 85px;'+
+'    }</style>');
+    mywindow.document.write('</head><body>');
+    mywindow.document.write("<img src='"+document.location.origin+"/static/images/logo/logoMpM.png' />");
+    mywindow.document.write('<h2>'+title+'</h2>');
+    mywindow.document.write(data);
+    mywindow.document.write('</body></html>');
+    mywindow.document.close();
+    mywindow.focus();
+    mywindow.print();
+    mywindow.close();
+    return true;
+}
