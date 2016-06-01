fb5840cf4c08ecf9e70c2dd1631295a4631f7de8
Gustavo Furtado <gustavo.foa@gmail.com>
2016-06-01 20:28:47 -0300

    Add line border on search results

--- apps/mpm/views/view_search.py
+++ apps/mpm/views/view_search.py
@@ -7,12 +7,4 @@ from django.db.models import Q
 def search(request):
     ctx = base_context()
 
-    #s = request.GET.get('s', None)
-
-    #categorias = Categoria.objects.filter(Q(nome__icontains=s) | Q(descricao__icontains=s))[:10]
-    #ctx['categorias'] = categorias
-
-    #musicas = Musica.objects.filter(Q(nome__icontains=s) | Q(letra__icontains=s))[:10]
-    #ctx['musicas'] = musicas
-
     return render(request, 'search.html', ctx)
