a1ba7117795faa1806f0ea9b4355115d2b6c49a5
Gustavo Furtado <gustavo.foa@gmail.com>
2016-06-26 13:15:49 -0300

    New images for sugestoes-para pages

--- apps/mpm/urls.py
+++ apps/mpm/urls.py
@@ -22,15 +22,15 @@ urlpatterns = [
     url(r'^confirme-seu-email/$', TemplateView.as_view(template_name='mail/confirme-seu-email.html', content_type='text/html')),
     url(r'^assinatura-confirmada/$', TemplateView.as_view(template_name='mail/assinatura-confirmada.html', content_type='text/html')),
     url(r'^desinscricao/$', TemplateView.as_view(template_name='mail/desinscricao.html', content_type='text/html')),
+    url(r'^import-data/', view_import.import_data, name='import-data'),
     url(r'^starratings-ajax.do$', view_starratings.starratings_ajax, name='starratings-ajax'),
     url(r'^datas.json$', view_datas.datas, name='datas'),
     url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
-    url(r'^(?P<slug>[-\w\d]+)/$', RedirectView.as_view(url='/sugestoes-para/%(slug)s')),
     url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    url(r'^(?P<slug>[-\w\d]+)/$', RedirectView.as_view(url='/sugestoes-para/%(slug)s')),
     url(r'^musica/(?P<slug>[-\w\d]+)/$', views.musica, name='musica'),
     url(r'^musicas-de/(?P<slug>[-\w\d]+)/$', views.musicas_de, name='musicas-de'),
     url(r'^sugestoes-para/(?P<slug>[-\w\d]+)/$', views.sugestoes_para, name='sugestoes-para'),
-    url(r'^import-data/', view_import.import_data, name='import-data'),
 ]
 
 
