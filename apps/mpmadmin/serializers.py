96bbb462356d70c4930d733061caf4e4738ad97e
Gustavo Furtado <gustavo.foa@gmail.com>
2016-07-02 16:08:36 -0300

    New images

--- apps/mpmadmin/serializers.py
+++ apps/mpmadmin/serializers.py
@@ -2,7 +2,7 @@ from ..mpm.models.Categoria import Categoria
 from rest_framework import serializers
 
 
-class CategoriaSerializer(serializers.HyperlinkedModelSerializer):
+class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categoria
         fields = ('slug', 'nome', 'descricao', 'categoria_mae', 'ordem')
