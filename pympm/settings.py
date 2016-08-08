337d824a79e7b66df73017aec1ddab02c0c69f26
Gustavo Furtado <gustavo.foa@gmail.com>
2016-08-08 19:55:54 -0300

    Images rename

--- pympm/settings.py
+++ pympm/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*3@=r_c6xobh80utpje=$&-v-u@)ofb56#cg(8^@l-jd-q0-u#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
