df0c839315b98bf876f51f1da6561f0781c26776
Gustavo Furtado <gustavo.foa@gmail.com>
2016-08-08 22:51:02 -0300

    Fix wsgi bug

--- pympm/wsgi.py
+++ pympm/wsgi.py
@@ -7,8 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-import os, sys
-
+import os
+import time
+import traceback
+import signal
+import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pympm.settings")
