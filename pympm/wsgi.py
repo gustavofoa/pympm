e84c6453eaefea852f12fa594cdb8b5274a4fe60
Gustavo Furtado <gustavo.foa@gmail.com>
2016-08-08 21:51:11 -0300

    Fix WSGI bug

--- pympm/wsgi.py
+++ pympm/wsgi.py
@@ -1,10 +1,10 @@
 """
-WSGI config for project pympm.
+WSGI config for pympm project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
@@ -13,4 +13,15 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pympm.settings")
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
+
+try:
+    application = get_wsgi_application()
+    print 'WSGI without exception'
+except Exception:
+    print 'handling WSGI exception'
+    # Error loading applications
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
