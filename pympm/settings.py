6c26c32da23c74414ca86b93220cbc5fb11a1d22
gustavofoa <gustavo.foa@gmail.com>
2016-10-09 19:14:57 -0300

    Change port on CORS Whitelist

--- pympm/settings.py
+++ pympm/settings.py
@@ -67,8 +67,8 @@ MIDDLEWARE_CLASSES = (
 CORS_ORIGIN_WHITELIST = (
     'musicasparamissa.com.br',
     'blog.musicasparamissa.com.br',
-    'localhost:8000',
-    '127.0.0.1:8000'
+    'localhost:9000',
+    '127.0.0.1:9000'
 )
 
 ROOT_URLCONF = 'pympm.urls'
