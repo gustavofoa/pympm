96c156f0beec51e28a8e73941a997701441c487c
gustavofoa <gustavo.foa@gmail.com>
2016-11-07 19:11:16 -0200

    Fix ending slash bug

--- apps/mpm/urls.py
+++ apps/mpm/urls.py
@@ -20,18 +20,18 @@ sitemaps = {
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^search$', search, name='search'),
-    url(r'^confirme-seu-email/$', TemplateView.as_view(template_name='mail/confirme-seu-email.html', content_type='text/html')),
-    url(r'^assinatura-confirmada/$', TemplateView.as_view(template_name='mail/assinatura-confirmada.html', content_type='text/html')),
-    url(r'^desinscricao/$', TemplateView.as_view(template_name='mail/desinscricao.html', content_type='text/html')),
-    url(r'^import-data/', import_data, name='import-data'),
+    url(r'^confirme-seu-email/?$', TemplateView.as_view(template_name='mail/confirme-seu-email.html', content_type='text/html')),
+    url(r'^assinatura-confirmada/?$', TemplateView.as_view(template_name='mail/assinatura-confirmada.html', content_type='text/html')),
+    url(r'^desinscricao/?$', TemplateView.as_view(template_name='mail/desinscricao.html', content_type='text/html')),
+    url(r'^import-data/?', import_data, name='import-data'),
     url(r'^starratings-ajax.do$', starratings_ajax, name='starratings-ajax'),
     url(r'^datas.json$', datas, name='datas'),
     url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
-    url(r'^(?P<slug>[-\w\d]+)/$', RedirectView.as_view(url='/sugestoes-para/%(slug)s')),
-    url(r'^musica/(?P<slug>[-\w\d]+)/$', views.musica, name='musica'),
-    url(r'^musicas-de/(?P<slug>[-\w\d]+)/$', views.musicas_de, name='musicas-de'),
-    url(r'^sugestoes-para/(?P<slug>[-\w\d]+)/$', views.sugestoes_para, name='sugestoes-para'),
+    url(r'^(?P<slug>[-\w\d]+)/?$', RedirectView.as_view(url='/sugestoes-para/%(slug)s')),
+    url(r'^musica/(?P<slug>[-\w\d]+)/?$', views.musica, name='musica'),
+    url(r'^musicas-de/(?P<slug>[-\w\d]+)/?$', views.musicas_de, name='musicas-de'),
+    url(r'^sugestoes-para/(?P<slug>[-\w\d]+)/?$', views.sugestoes_para, name='sugestoes-para'),
 ]
 
 
