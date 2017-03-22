86bbeb1ef6c3bfe02ed72e0f0b5a8d1394a7da75
Gustavo Furtado <gustavo.foa@gmail.com>
2017-03-22 20:11:07 -0300

    Use static from S3

--- pympm/settings.py
+++ pympm/settings.py
@@ -136,8 +136,8 @@ AWS_STORAGE_BUCKET_NAME = 'pympm'
 # it simple - just use this domain plus the path. (If this isn't set, things get complicated).
 # This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
 # We also use it in the next setting.
-# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_CUSTOM_DOMAIN = 'dm8sjho5caga5.cloudfront.net'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+#AWS_S3_CUSTOM_DOMAIN = 'dm8sjho5caga5.cloudfront.net'
 
 # This is used by the `static` template tag from `static`, if you're using that. Or if anything else
 # refers directly to STATIC_URL. So it's safest to always set it.
