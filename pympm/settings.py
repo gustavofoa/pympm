304d4761687bfc54bf565f4ebf6d6357b4e7319b
Gustavo Furtado de Oliveira Alves <gustavo.foa@gmail.com>
2017-07-09 15:04:25 -0300

    Temporary change static files to HTTP

--- pympm/settings.py
+++ pympm/settings.py
@@ -122,31 +122,31 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # AWS_IS_GZIPPED = False
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY', 'SETUP AWS_ACCESS_KEY'),
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_KEY', 'SETUP AWS_SECRET_KEY'),
+# AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY', 'SETUP AWS_ACCESS_KEY'),
+# AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_KEY', 'SETUP AWS_SECRET_KEY'),
 
-AWS_HEADERS = {
-    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-    'Cache-Control': 'max-age=94608000',
-}
+# AWS_HEADERS = {
+#     'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+#     'Cache-Control': 'max-age=94608000',
+# }
 
 #AWS_STORAGE_BUCKET_NAME = 'pympm'
-AWS_STORAGE_BUCKET_NAME = 'static.musicasparamissa.com.br'
+# AWS_STORAGE_BUCKET_NAME = 'static.musicasparamissa.com.br'
 
 # Tell django-storages that when coming up with the URL for an item in S3 storage, keep
 # it simple - just use this domain plus the path. (If this isn't set, things get complicated).
 # This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
 # We also use it in the next setting.
 # AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_CUSTOM_DOMAIN = 'static.musicasparamissa.com.br'
+# AWS_S3_CUSTOM_DOMAIN = 'static.musicasparamissa.com.br'
 
 # This is used by the `static` template tag from `static`, if you're using that. Or if anything else
 # refers directly to STATIC_URL. So it's safest to always set it.
-STATIC_URL = "http://%s/" % AWS_S3_CUSTOM_DOMAIN
+STATIC_URL = "http://static.musicasparamissa.com.br/"
 
 # Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
 # you run `collectstatic`).
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 # STATICFILES_STORAGE = 'precompressed.storage.s3boto.PrecompressedS3BotoStorage'
 
 # Internationalization
