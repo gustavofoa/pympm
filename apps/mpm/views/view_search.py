3f09ece03f4741ce95ec954e44e4fc3b4a88d199
Gustavo Furtado de Oliveira Alves <gustavo.alves@mult-e.com.br>
2017-09-11 19:18:32 -0300

    Fix search view

--- apps/mpm/views/view_search.py
+++ apps/mpm/views/view_search.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .views import base_context
-from ..models import Categoria, Musica
-from django.db.models import Q
 
 def search(request):
     ctx = base_context()
